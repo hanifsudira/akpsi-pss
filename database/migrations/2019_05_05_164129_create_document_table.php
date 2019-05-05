65e7dd2bc836eaf018bfb674510c21d2d560f217
Alifiannisa Lawami Diar <alifiannisald@gmail.com>
2019-05-05 23:56:21 +0700

    test db

---
+++ database/migrations/2019_05_05_164129_create_document_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('partner_name', 50);
+            $table->integer('upload_by');
+            $table->timestamps('upload_time');
+            $table->string('document_name', 255);
+            $table->binary('document_file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document');
+    }
+}
---
+++ database/seeds/user.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+         DB::table('users')->insert([
+            'id_role' => '1',
+            'name' => 'Rektor UI',
+			'username' => '1',
+			'password' => '1',
+		 	'last_login' => '1',
+		 	'role' => 'delivery'
+		 ]); 
+
+    }
+}
