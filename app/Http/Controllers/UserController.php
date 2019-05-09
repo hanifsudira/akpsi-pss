ef656eee014c21a4d568c668c640368b42132ae6
hilwis <hilman.wisnu@ovo.id>
2019-05-09 08:34:37 +0700

    advance multiple search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\AddUser;
+use Illuminate\Support\Facades\DB;
+use Yajra\Datatables\Datatables;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,5 +20,51 @@ class UserController extends Controller
 
     public function searchresult(){
         return view('user.searchresult');
+    }
+
+    public function getAdvanceFilterData(Request $request){
+        $invoice = DB::table('proposed_invoice')->select([
+            'proposed_invoice.id',
+            'proposed_invoice.partner_name',
+            'proposed_invoice.product',
+            'proposed_invoice.contract_number as invoice_number',
+            'status.remarks',
+            'proposed_invoice.contract_number',
+            'proposed_invoice.product',
+            'proposed_invoice.id_bg_sum as skema',
+            'proposed_invoice.created_at as contract_start',
+            'proposed_invoice.id_doc as document_attachment',
+            'proposed_invoice.id_status as payment_status'
+            
+        ])->leftJoin('status','status.id','=','proposed_invoice.id_status');
+
+        $datatables =  Datatables::of($invoice);
+
+        if ($name = $request->get('partner_name')) {
+            // additional users.name search
+            $invoice->where('proposed_invoice.partner_name', 'like', "$name%");
+        }
+
+        if ($invoice_num = $request->get('invoice_number')) {
+            $invoice->where('invoice_number', 'like', "$invoice_num%");
+        }
+
+        if ($status = $request->get('status')) {
+            $invoice->where('status.remarks', 'like', $request->get('status'));
+        }
+
+        if ($keyword = $request->get('search')['value']) {
+            // override users.name global search
+            $datatables->filterColumn('proposed_invoice.partner_name', 'where', 'like', "$keyword%");
+
+            // override users.id global search - demo for concat
+            $datatables->filterColumn('proposed_invoice.id', 'where', "like", "%$keyword%");
+
+            
+        }
+
+        return $datatables->make(true);
+
+
     }
 }
